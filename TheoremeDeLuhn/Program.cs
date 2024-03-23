7fff8f6b74f189c1fa5650f081bfaef0e5850392
fred <domaine75@gmail.com>
2024-03-24 00:35:03 +0100

    Ajoutez des fichiers projet.

---
+++ TheoremeDeLuhn/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheoremeDeLuhn
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      long[] testNumbers = { 49927398716, 49927398717, 1234567812345678, 1234567812345670 };
+      foreach (var testNumber in testNumbers)
+        Console.WriteLine("{0} is {1}valid", testNumber, testNumber.ToString().LuhnCheck() ? "" : "not ");
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
