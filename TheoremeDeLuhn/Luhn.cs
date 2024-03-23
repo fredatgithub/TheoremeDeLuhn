bd044bafe1a2295c45987ac477eb6fa09fc6b58a
fred <domaine75@gmail.com>
2024-03-24 00:43:18 +0100

    add two methods

--- TheoremeDeLuhn/Luhn.cs
+++ TheoremeDeLuhn/Luhn.cs
@@ -18,5 +18,32 @@ namespace TheoremeDeLuhn
     {
       return digits.Select((d, i) => i % 2 == digits.Length % 2 ? ((2 * d) % 10) + d / 5 : d).Sum() % 10;
     }
+
+    public static bool IsLuhn(long n)
+    {
+      long nextdigit, sum = 0;
+      bool alt = false;
+      while (n != 0)
+      {
+        nextdigit = n % 10;
+        if (alt)
+        {
+          nextdigit *= 2;
+          nextdigit -= (nextdigit > 9) ? 9 : 0;
+        }
+
+        sum += nextdigit;
+        alt = !alt;
+        n /= 10;
+      }
+
+      return (sum % 10 == 0);
+    }
+
+    public static bool LuhnLinq(long number)
+    {
+      string s = number.ToString();
+      return s.Select((c, i) => (c - '0') << ((s.Length - i - 1) & 1)).Sum(n => n > 9 ? n - 9 : n) % 10 == 0;
+    }
   }
 }
