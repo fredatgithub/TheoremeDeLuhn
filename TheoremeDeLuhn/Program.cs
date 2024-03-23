2f7a2fe8ed6531d282178fe3fbbdb27de63fc78e
fred <domaine75@gmail.com>
2024-03-24 00:36:15 +0100

    clean code

--- TheoremeDeLuhn/Program.cs
+++ TheoremeDeLuhn/Program.cs
@@ -6,9 +6,16 @@ namespace TheoremeDeLuhn
   {
     static void Main()
     {
-      long[] testNumbers = { 49927398716, 49927398717, 1234567812345678, 1234567812345670 };
+      long[] testNumbers = { 
+        49927398716, 
+        49927398717, 
+        1234567812345678, 
+        1234567812345670 };
+
       foreach (var testNumber in testNumbers)
+      {
         Console.WriteLine("{0} is {1}valid", testNumber, testNumber.ToString().LuhnCheck() ? "" : "not ");
+      }
 
       Console.WriteLine("Press any key to exit:");
       Console.ReadKey();
